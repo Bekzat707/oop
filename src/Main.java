d571d2b9621f531f662ad22c08c0fb6344a4200a
bekzatsaparbekov <bekzatsaparbekov070@gmail.com>
2024-12-07 14:56:56 +0600

    unit commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Bekzat Saparbekov");
+        System.out.printf("Bekzat Saparbekov Kbtushnik");
     }
 }
